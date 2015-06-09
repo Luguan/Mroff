14fbf383bbb4d900cd0199a70bb5479e9d643fdc
Lukas <lulleannsberg@gmail.com>
2015-06-09 15:31:56 +0200

    created project

---
+++ desktop/src/com/Luguan/Mroff/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.Luguan.Mroff.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.Luguan.Mroff.Mroff;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Mroff(), config);
+	}
+}
