5e15f3ce6b148063faed36176a63ca9af0cf4b84
Henrik Karlsson <hk.henrik@gmail.com>
2015-06-12 16:25:49 +0200

    Autobuild web

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env sh
 
-export source='./desktop/build/libs/desktop-1.0.jar'
+export desktopJar='./desktop/build/libs/desktop-1.0.jar'
+export htmlFolder='./html/build/dist'
 export version='0.1-pre'
 
 shortHash() {
@@ -10,11 +11,33 @@ shortHash() {
 commitCount() {
     git rev-list HEAD --count
 }
+
 fileName() {
     echo mroff-$(shortHash).jar
 }
 
-./gradlew desktop:dist
-mkdir release
-mv ${source} release/$(fileName)
-./gh-release create Luguan/Mroff ${version}-b$(commitCount)
+createDesktopDist() {
+    ./gradlew desktop:dist
+    mkdir release
+    mv ${desktopJar} release/$(fileName)
+    ./gh-release create Luguan/Mroff ${version}-b$(commitCount)
+}
+
+createWebDist() {
+    git config user.name "Circle CI"
+    git config user.email hk.henrik+circle@gmail.com
+    git remote add origin git@github.com:Ineentho/blog.git
+
+    ./gradlew html:dist
+    git clone git@github.com:Luguan/Mroff.git --branch gh-pages --single-branch gh-pages
+    mkidr game
+    cd game
+    rm -rf .
+    mv ../../${htmlFolder} .
+    git add -A
+    git commit -m "Build #$(commitCount)"
+    git push
+}
+
+createDesktopDist
+createWebDist
