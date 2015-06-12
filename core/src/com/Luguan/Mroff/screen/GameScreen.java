557f53be9aa03861947dfaa5a630249165721e64
Lukas <lulleannsberg@gmail.com>
2015-06-12 14:17:43 +0200

    removed unused imports

--- core/src/com/Luguan/Mroff/screen/GameScreen.java
+++ core/src/com/Luguan/Mroff/screen/GameScreen.java
@@ -4,17 +4,11 @@ import com.Luguan.Mroff.Mroff;
 import com.Luguan.Mroff.character.Character;
 import com.Luguan.Mroff.util.Utils;
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.Input;
 import com.badlogic.gdx.ScreenAdapter;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
-import com.badlogic.gdx.maps.MapLayer;
-import com.badlogic.gdx.maps.MapObjects;
-import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
 import com.badlogic.gdx.math.Vector2;
-import sun.plugin.javascript.navig4.Layer;
-
 
 /**
  * Created by Lukas on 6/9/2015.
@@ -58,7 +52,6 @@ public class GameScreen extends ScreenAdapter{
 
     @Override
     public void render(float delta) {
-
         Gdx.gl.glClearColor(1, 1, 1, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
