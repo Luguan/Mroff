98585296c22b86db95960763b0d59d6dcb6d342b
Henrik Karlsson <hk.henrik@gmail.com>
2015-06-12 20:41:55 +0200

    Better menu system

--- core/src/com/Luguan/Mroff/screen/MenuScreen.java
+++ core/src/com/Luguan/Mroff/screen/MenuScreen.java
@@ -1,113 +1,39 @@
 package com.Luguan.Mroff.screen;
 
-import com.Luguan.Mroff.Mroff;
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.ScreenAdapter;
-import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.scenes.scene2d.*;
 import com.badlogic.gdx.scenes.scene2d.ui.Button;
-import com.badlogic.gdx.scenes.scene2d.ui.Skin;
-import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
-import com.badlogic.gdx.utils.Align;
-import com.badlogic.gdx.utils.viewport.ScreenViewport;
 
-public class MenuScreen extends ScreenAdapter {
+import java.util.ArrayList;
+import java.util.List;
 
-	private final Stage stage;
-	private final Skin skin;
+public class MenuScreen extends CenteredMenu {
+
 	private MenuAction action;
-	private Group grp;
 
-	public MenuScreen(final MenuAction action) {
+	public MenuScreen(MenuAction action) {
 		this.action = action;
-
-		stage = new Stage(new ScreenViewport());
-		Gdx.input.setInputProcessor(stage);
-		skin = Mroff.getInstance().getSkin();
-
-		Group grp = createMainMenu();
-		stage.addActor(grp);
-	}
-
-	private Group createMainMenu() {
-		grp = new Group();
-
-		Button bttn = createButton("New Game", 0);
-		bttn.addListener(new EventListener() {
-			@Override
-			public boolean handle(Event event) {
-				if (event instanceof InputEvent) {
-					InputEvent inputEvent = (InputEvent) event;
-					if (inputEvent.getType() == InputEvent.Type.touchUp && !inputEvent.isTouchFocusCancel()) {
-						action.newGame();
-						return true;
-					}
-				}
-				return true;
-
-			}
-		});
-		grp.addActor(bttn);
-
-		bttn = createButton("Load Game", bttn.getY() - bttn.getHeight() * .5f);
-		grp.addActor(bttn);
-
-		bttn = createButton("Settings", bttn.getY() - bttn.getHeight() * .5f);
-		grp.addActor(bttn);
-
-		bttn = createButton("Load custom map", bttn.getY() - bttn.getHeight() * .5f);
-		grp.addActor(bttn);
-
-		bttn = createButton("Full Screen", bttn.getY() - bttn.getHeight() * .5f);
-		bttn.addListener(new EventListener() {
-			@Override
-			public boolean handle(Event event) {
-				if (event instanceof InputEvent) {
-					InputEvent inputEvent = (InputEvent) event;
-					if (inputEvent.getType() == InputEvent.Type.touchUp && !inputEvent.isTouchFocusCancel()) {
-						Gdx.graphics.setDisplayMode(Gdx.graphics.getDisplayModes()[0]);
-						return true;
-					}
-				}
-				return true;
-			}
-		});
-		grp.addActor(bttn);
-		grp.setHeight(bttn.getY());
-		return grp;
-	}
-
-	private Button createButton(String label, float y) {
-		TextButton button = new TextButton(label, skin);
-
-		button.setPosition(0, y, Align.center);
-		return button;
 	}
 
 	@Override
-	public void resize(int width, int height) {
-		stage.getViewport().update(width, height, true);
-
-		grp.setY(Gdx.graphics.getHeight() / 2 - grp.getHeight() / 2);
-		grp.setX(Gdx.graphics.getWidth() / 2);
-
-		super.resize(width, height);
+	protected List<Button> getButtons() {
+		List<Button> bttns = new ArrayList<Button>();
+		bttns.add(createButton("New Game"));
+		bttns.add(createButton("Load Game"));
+		bttns.add(createButton("Settings"));
+		bttns.add(createButton("Load custom map"));
+		bttns.add(createButton("Full Screen"));
+		return bttns;
 	}
 
 	@Override
-	public void render(float delta) {
-		Gdx.gl.glClearColor(0, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		stage.act();
-		stage.draw();
-	}
-
-	@Override
-	public void dispose() {
-		stage.dispose();
-	}
-
-	public interface MenuAction {
-		void newGame();
+	public void buttonClick(String button) {
+		switch (button) {
+			case "New Game":
+				action.newGame();
+				break;
+			case "Full Screen":
+				Gdx.graphics.setDisplayMode(Gdx.graphics.getDisplayModes()[0]);
+				break;
+		}
 	}
 }
---
+++ core/src/com/Luguan/Mroff/screen/PauseMenuScreen.java
@@ -0,0 +1,5 @@
+package com.Luguan.Mroff.screen;
+
+public class PauseMenuScreen {
+
+}
