6be1a97ed59ccd720980cb4849494429b522af56
Henrik Karlsson <hk.henrik@gmail.com>
2015-06-12 16:46:28 +0200

    Go into to the clone gh-pages dir

--- deploy.sh
+++ deploy.sh
@@ -29,6 +29,7 @@ createWebDist() {
 
     ./gradlew html:dist
     git clone git@github.com:Luguan/Mroff.git --branch gh-pages --single-branch gh-pages
+    cd gh-pages
     mkdir game
     cd game
     rm -rf ./*
