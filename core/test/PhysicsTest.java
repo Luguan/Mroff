8a2f431d77c39b221043500818b53a776bc59414
Henrik Karlsson <hk.henrik@gmail.com>
2015-06-17 17:03:44 +0200

    Working tests

--- core/test/PhysicsTest.java
+++ core/test/PhysicsTest.java
@@ -8,44 +8,18 @@ import static junit.framework.TestCase.assertEquals;
 
 public class PhysicsTest {
 	@Test
-	public void collideTop() {
+	public void collideA() {
 		Rectangle r1 = new Rectangle(10, 10, 10, 10);
 		Rectangle r2 = new Rectangle(1, 1, 10, 10);
 		Vector2 collision = Collision.intersects(r1, r2);
-		System.out.println(collision);
-		//assertEquals(collision, Side.TOP);
-	}
-/*
-	@Test
-	public void collideLeft() {
-		Rectangle r1 = new Rectangle(9, 0, 10, 10);
-		Rectangle r2 = new Rectangle(0, 0, 10, 10);
-		Side side = Collision.intersects(r1, r2);
-		assertEquals(side, Side.LEFT);
+		assertEquals(collision, new Vector2(1, 1));
 	}
 
-	@Test
-	public void collideRight() {
-		Rectangle r1 = new Rectangle(0, 0, 10, 10);
-		Rectangle r2 = new Rectangle(9, 0, 10, 10);
-		Side side = Collision.intersects(r1, r2);
-		assertEquals(side, Side.RIGHT);
-	}
-
-	@Test
-	public void collideBottom() {
-		Rectangle r1 = new Rectangle(0, 9, 10, 10);
-		Rectangle r2 = new Rectangle(0, 0, 10, 10);
-		Side side = Collision.intersects(r1, r2);
-		assertEquals(side, Side.BOTTOM);
-	}
-*/
 	@Test
 	public void collideNothing() {
 		Rectangle r1 = new Rectangle(0, 0, 10, 10);
 		Rectangle r2 = new Rectangle(100, 100, 10, 10);
 		Vector2 collision = Collision.intersects(r1, r2);
-		System.out.println(collision);
 		assertEquals(collision, new Vector2());
 	}
 }
