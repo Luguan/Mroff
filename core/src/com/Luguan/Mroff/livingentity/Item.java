5c1972eb466b94a435c224d0c14af9e83970bc17
Lukas <lulleannsberg@gmail.com>
2015-07-10 19:15:04 +0200

    missed files

---
+++ core/src/com/Luguan/Mroff/livingentity/Item.java
@@ -0,0 +1,25 @@
+package com.Luguan.Mroff.livingentity;
+
+import com.Luguan.Mroff.Mroff;
+import com.Luguan.Mroff.physics.ObjectPhysics;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Batch;
+
+/**
+ * Created by Lukas on 6/28/2015.
+ */
+public class Item extends ObjectPhysics{
+    Texture item;
+    public Item() {
+        item = Mroff.getInstance().getTexture("items/Mushroom");
+    }
+
+    public void draw(Batch batch) {
+        batch.begin();
+        batch.draw(item,x,y);
+        batch.end();
+    }
+
+
+
+}
