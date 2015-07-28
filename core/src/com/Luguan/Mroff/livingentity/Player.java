e22da0d249ccd7d54fe091a5f31b2970ca550680
Lukas <lulleannsberg@gmail.com>
2015-07-28 18:04:00 +0200

    fully working animation

--- core/src/com/Luguan/Mroff/livingentity/Player.java
+++ core/src/com/Luguan/Mroff/livingentity/Player.java
@@ -14,22 +14,64 @@ public class Player extends LivingEntity{
     public static final float MOVEMENT_SPEED = 0.15f;
     PlayerState playerState;
 
+    boolean lookingRight;
+
+    int spriteFrame;
+    float frameTime;
+
     public Player(CollisionEvent event) {
         super(new Sprite(Mroff.getInstance().getSheet(0, 0)), event);
+
+        spriteFrame = 0;
+
+        lookingRight = true;
 
         width = 3/4f;
         height = 4/4f;
         getSprite().setSize(width,height);
     }
 
+
+
     @Override
     public void update(float delta) {
+        frameTime+=delta;
         super.update(delta);
         if(Gdx.input.isKeyPressed(Input.Keys.A)) {
+            if(frameTime>0.3f) {
+                frameTime = 0;
+                if(spriteFrame == 0) {
+                    spriteFrame++;
+                }
+                else {
+                    spriteFrame = 0;
+                }
+                getSprite().setRegion(Mroff.getInstance().getSheet(0, spriteFrame));
+            }
+
             x -= MOVEMENT_SPEED;
+            if(lookingRight) {
+                getSprite().flip(true, false);
+                lookingRight=false;
+            }
         }
         if(Gdx.input.isKeyPressed(Input.Keys.D)) {
+            if(frameTime>0.3f) {
+                frameTime = 0;
+                if(spriteFrame == 0) {
+                    spriteFrame++;
+                }
+                else {
+                    spriteFrame = 0;
+                }
+                getSprite().setRegion(Mroff.getInstance().getSheet(0, spriteFrame));
+            }
+
             x += MOVEMENT_SPEED;
+            if(!lookingRight) {
+                getSprite().flip(true, false);
+                lookingRight = true;
+            }
         }
         if(Gdx.input.isKeyPressed(Input.Keys.W)) {
             if(!inAir) {
